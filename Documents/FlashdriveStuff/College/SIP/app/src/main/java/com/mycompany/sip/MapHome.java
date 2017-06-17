e87a6033807e471bac1c7f3ea4cfb6150cb02ea8
Emily Fletcher <EFletcher2014@gmail.com>
2017-06-17 00:58:22 -0400

    first commit, just simple layout

---
+++ Documents/FlashdriveStuff/College/SIP/app/src/main/java/com/mycompany/sip/MapHome.java
@@ -0,0 +1,13 @@
+package com.mycompany.sip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_home);
+    }
+}
