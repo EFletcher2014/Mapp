4904fcc15966c6e532ad59d64f2623070fbbf03f
Emily Fletcher <EFletcher2014@gmail.com>
2017-06-22 16:19:44 -0400

    got image selection and display to work, replaced a textview with the imageview

--- Documents/FlashdriveStuff/College/SIP/app/src/main/java/com/mycompany/sip/MapHome.java
+++ Documents/FlashdriveStuff/College/SIP/app/src/main/java/com/mycompany/sip/MapHome.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 public class MapHome extends AppCompatActivity {
@@ -22,8 +23,8 @@ public class MapHome extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map_home);
 
-        final TextView pictureGetter = (TextView) findViewById(R.id.get_picture);
-        pictureGetter.setOnClickListener(new View.OnClickListener() {
+        final ImageView unitImage = (ImageView) findViewById(R.id.unitImgView);
+        unitImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Get Picture
@@ -41,45 +42,15 @@ public class MapHome extends AppCompatActivity {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 Uri selectedImageUri = data.getData();
+                System.out.println("selectedImageUri is" + selectedImageUri);
 
                 //OI FILE Manager
                 filemanagerstring = selectedImageUri.getPath();
 
                 //MEDIA GALLERY
-                selectedImagePath = getPath(selectedImageUri);
-
-                //DEBUG PURPOSE - you can delete this if you want
-                if (selectedImagePath != null)
-                    System.out.println(selectedImagePath);
-                else System.out.println("selectedImagePath is null");
-                if (filemanagerstring != null)
-                    System.out.println(filemanagerstring);
-                else System.out.println("filemanagerstring is null");
-
-                //NOW WE HAVE OUR WANTED STRING
-                if (selectedImagePath != null)
-                    System.out.println("selectedImagePath is the right one for you!");
-                else
-                    System.out.println("filemanagerstring is the right one for you!");
+                ImageView unitImage = (ImageView) findViewById(R.id.unitImgView);
+                unitImage.setImageURI(selectedImageUri);
             }
         }
-    }
-    /**
-     * FROM STACKOVERFLOW https://stackoverflow.com/questions/2169649/get-pick-an-image-from-androids-built-in-gallery-app-programmatically*****
-     * helper to retrieve the path of an image URI
-     */
-    public String getPath(Uri uri) {
-        String[] projection = { MediaStore.Images.Media.DATA };
-        Cursor cursor = getContentResolver().query(uri, projection, null, null, null);
-        if(cursor!=null)
-        {
-            //HERE YOU WILL GET A NULLPOINTER IF CURSOR IS NULL
-            //THIS CAN BE, IF YOU USED OI FILE MANAGER FOR PICKING THE MEDIA
-            int column_index = cursor
-                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
-            cursor.moveToFirst();
-            return cursor.getString(column_index);
-        }
-        else return null;
     }
 }
