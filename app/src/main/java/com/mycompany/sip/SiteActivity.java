2ccc42e577fccf1d7afa89bd6d5b4c5b06be563c
Emily Fletcher <EFletcher2014@gmail.com>
2018-09-24 22:58:51 -0400

    Site loading and creation is working again, but need to fix the userHasWritePermission activities

---
+++ app/src/main/java/com/mycompany/sip/SiteActivity.java
@@ -0,0 +1,13 @@
+package com.mycompany.sip;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SiteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_site);
+    }
+}
